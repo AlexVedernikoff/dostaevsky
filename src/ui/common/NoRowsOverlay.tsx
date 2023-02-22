945d95d9ca5f2798afc4fe1301e1ad047628f851
AlexVedernikoff <templar_sc@mail.ru>
2023-02-22 10:58:51 +0300

    функционал к кнопке Сбросить фильтры

--- src/ui/common/NoRowsOverlay.tsx
+++ src/ui/common/NoRowsOverlay.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "@mui/material";
 import { ReactComponent as Magnifier } from "../../../src/svg/magnifier.svg";
 import { ReactComponent as CrossMagnifier } from "../../svg/crossMagnifier.svg";
+import { resetState } from "../../store/mainState";
 
 export function NoRowsOverlay() {
+    const dispatch = useDispatch();
+
+    console.log("resetState = ", resetState)
     return (
         <Stack height="100%" alignItems="center" justifyContent="center">
             <div className="IconSearch">
@@ -24,7 +29,10 @@ export function NoRowsOverlay() {
                 Измените или сбросьте примененные фильтры
             </p>
             <div className="button">
-                <Button variant="outlined">Сбросить фильтры</Button>
+                <Button onClick={() => {
+                    console.log("Вы сбросили фильтры до значений по умолчанию")
+                    dispatch(resetState());
+                }} variant="outlined">Сбросить фильтры</Button>
             </div>
         </Stack>
     );
