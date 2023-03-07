aef48a893fd4579fcbf02330cf1a62de8a5731d7
AlexVedernikoff <templar_sc@mail.ru>
2023-03-07 23:41:57 +0300

    меняем обратно index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,27 @@
 import axios from "axios";
 
-// export const HOST_URL = https://online.staging.dostaevsky.ru
-export const HOST_URL = "https://dost.strio.ru"
-export const API_URL = HOST_URL + "/operator/api";
-export const SINGNIN_URL = HOST_URL + "/signin";
-
-console.log("API_URL = ", API_URL)
+export const HOST_URL = `https://online.staging.dostaevsky.ru`
+export const API_URL = HOST_URL + `/operator/api`;
+export const SIGNIN_URL = HOST_URL + `/signin`;
 
 const api = axios.create({
-    //withCredentials: true,
+    withCredentials: true,
     baseURL: API_URL,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
     }
 });
-/*
+
 api.interceptors.request.use((config) => {
     let tokens = JSON.parse(localStorage.getItem("tokens"));
-    config.headers.Authorization = Bearer ${tokens.id_token};
+    if (!tokens) {
+        window.location.href = SIGNIN_URL;
+    } else {
+        config.headers.Authorization = `Bearer ${tokens.access_token}`;
+    }
     return config;
 });
 
@@ -33,24 +34,21 @@ api.interceptors.response.use(
         if (error.response.status === 401 && error.config && !error.config._isRetry) {
             originalRequest._isRetry = true;
             try {
-                const response = await api.get(/auth/refresh);
+                const response = await api.post(`/auth/refresh`);
                 let tokens = JSON.parse(localStorage.getItem("tokens"));
                 if (!tokens) tokens = {};
 
-                tokens.id_token = response.data.id_token;
+                tokens = response.data;
 
                 localStorage.setItem("tokens", JSON.stringify(tokens));
 
                 return api.request(originalRequest);
             } catch (e) {
-                console.log("пользователь не авторизирован")
-
-                window.location.href = SINGNIN_URL;
+                window.location.href = SIGNIN_URL;
             }
         }
         throw error;
     }
 );
-*/
-export default api;
 
+export default api;
