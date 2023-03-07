09606ff16fb16f8dbe3fef133923ae07a18f7583
AlexVedernikoff <templar_sc@mail.ru>
2023-03-07 15:44:04 +0300

    фиксим баг 18  дублирующиеся запросы

--- src/ui/common/CustomSelect.tsx
+++ src/ui/common/CustomSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useAppDispatch } from "hooks/use-redux";
 import Select from "react-select";
 import { MultiValue, InputOption } from ".";
@@ -11,14 +11,14 @@ import { data } from "../../data/dataOrder";
 export const CustomSelect = (props: any) => {
     console.log("Рендер компонента CustomSelect")
 
-
     const dispatch = useAppDispatch();
     const options = props.multi ? (props.options.length !== props.defaultValue.length ? [data.typeSelect.ALL, ...props.options] : [data.typeSelect.RESET, ...props.options]) : props.options;
     const [selectedOptions, setSelected] = useState(props.defaultValue);
-    // const [selectedOptions, setSelected] = useState([]);
-    console.log("selectedOptions 17 = ", selectedOptions)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    console.log("isModalOpen = ", isModalOpen)
+    // console.log("selectedOptions 17 = ", selectedOptions)
     function dispatchChanges(selected) {
-        console.log("Сработало событие dispatchChanges")
+        // console.log("Сработало событие dispatchChanges")
         if (!props.modal) {
             if (Array.isArray(selected)) {
                 props.multi && selected.length && selected.find((option) => option.value === 0)
@@ -41,12 +41,6 @@ export const CustomSelect = (props: any) => {
 
     const debouncedDispatchChange = debounce(dispatchChanges, 500)
 
-    useEffect(() => {
-        console.log("Сработал useEffect!!!!!")
-        console.log("selectedOptions = ", selectedOptions)
-        // debouncedDispatchChange(selectedOptions)
-    }, [selectedOptions])
-
     return (
         <Select
             options={options}
@@ -65,48 +59,36 @@ export const CustomSelect = (props: any) => {
             className={props.className}
             defaultValue={props.defaultValue}
             onMenuOpen={() => {
-
+                setIsModalOpen(true)
             }}
             onMenuClose={() => {
-                console.log("Сработало событие onMenuClose")
-
+                setIsModalOpen(false)
+                // console.log("Сработало событие onMenuClose")
                 const selected = selectedOptions
-                console.log("selectedOptions 64 = ", selectedOptions)
-                // setSelected((previous) => {
-                //     console.log("Предыдущее состояние = ", previous)
-
-                //     return selected
-                // });
-
-
+                // console.log("selectedOptions 64 = ", selectedOptions)
                 if (!selected.find((option) => option.value === 0 || option.value === -1)) {
                     console.log("Мы здесь!! onMenuClose")
-
-
                     debouncedDispatchChange(selected)
                 }
             }
             }
             onChange={(selected) => {
 
-                console.log("selected = ", selected)
-                for (let el of selected) {
-                    console.log(el);
-                }
-                setSelected(() => {
-                    console.log("Вызываем функцию setSelected()")
-                    return selected
-                });
-                console.log("Сработало событие onChange")
+                // console.log("selected = ", selected)
+                // for (let el of selected) {
+                //     console.log(el);
+                // }
+                setSelected(selected);
+                // console.log("Сработало событие onChange")
+                // console.log(props)
 
                 if (selected.find((option) => option.value === 0 || option.value === -1)) {
-                    console.log("Мы здесь!! onChange")
-
+                    // console.log("Мы здесь!! onChange")
                     debouncedDispatchChange(selected)
-                    // setSelected(() => {
-                    //     console.log("Вызываем функцию setSelected()")
-                    //     return selected
-                    // });
+                }
+                if (!isModalOpen) {
+                    // console.log("Мы здесь!! onChange")
+                    debouncedDispatchChange(selected)
                 }
             }}
         />
